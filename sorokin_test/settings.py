101e8bcfa02d094623385634dcced16ab6fedefd
Alexandr Sorokin <bsorokind@gmail.com>
2011-10-25 00:48:04 +0300

    ticket:4 context_processor

--- sorokin_test/settings.py
+++ sorokin_test/settings.py
@@ -23,6 +23,15 @@ DATABASES = {
     }
 }
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "django.contrib.messages.context_processors.messages",
+    'core.template_processor.settings_processor')
+
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
