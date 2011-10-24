0ed331061f3eb5d3b4c31fa4f933b69154114a3c
Alexandr Sorokin <bsorokind@gmail.com>
2011-10-25 01:45:46 +0300

    ticket:5 add view, layout, test

--- sorokin_test/contact/views.py
+++ sorokin_test/contact/views.py
@@ -1,10 +1,17 @@
 from django.views.generic import DetailView
 from models import Person, RequestStore
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, UpdateView
 
 
 class PersonDetailView(DetailView):
     context_object_name = "person"
+    model = Person
+
+    def get_object(self, **kwargs):
+        return self.model.objects.all()[0]
+
+
+class PersonEditView(UpdateView):
     model = Person
 
     def get_object(self, **kwargs):
