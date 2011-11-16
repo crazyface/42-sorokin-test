439d6f793f37ce73f8975e53245d50fddaaad6ff
Alexandr Sorokin <bsorokind@gmail.com>
2011-11-16 16:05:04 +0200

    ticket:9 custom django command + sh script

--- sorokin_test/core/management/commands/model_list.py
+++ sorokin_test/core/management/commands/model_list.py
@@ -1,16 +1,15 @@
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from django.db import models
 
+
 class Command(BaseCommand):
-    def __init__(self, *args, **kwargs):
-        super(Command, self).__init__(*args, **kwargs)
 
     def handle(self, *args, **options):
         model_list = []
-        if args:
-            for arg in args:
-                model_list.append(models.get_model(*arg.split('.')))
-        else:
+        for arg in args:
+            model_list.append(models.get_model(*arg.split('.')))
+        if not model_list:
             model_list = models.get_models()
         for model in model_list:
-            self.stdout.write('%s has %s objects\n' % (model, model.objects.count()))
+            self.stdout.write('%s has %s objects\n' % (model,
+                                                       model.objects.count()))
